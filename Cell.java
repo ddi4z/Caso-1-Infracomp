ae165211b3059b6c5cbf38d227c461d9e41dfa02
Daniel Diaz <110594320+ddi4z@users.noreply.github.com>
2024-02-11 13:21:48 -0500

    Primera version funcional

--- Cell.java
+++ Cell.java
@@ -3,19 +3,25 @@ import java.util.concurrent.CyclicBarrier;
 
 public class Cell extends Thread{
 
-
+    // Atributos basicos
     private boolean estado;
     private Mailbox mailbox;
 
+    private static int id = 1 ;
+    private int rol;
+
+    // Vecinos
     private ArrayList<Mailbox> vecinos;
     private int vecinosVivos;
 
+    // Atributos estaticos para las barreras
     private static int numeroGeneraciones;
     private static CyclicBarrier barreraGeneracion;
     private static CyclicBarrier barreraEstado;
 
+    // Atributo estatico para saber si se ha terminado
     private static boolean fin = false;
-    
+
 
 
 
@@ -23,10 +29,11 @@ public class Cell extends Thread{
         this.estado = false;
         this.mailbox = mailbox;
         this.vecinosVivos = 0;
-
+        this.rol = id;
+        id++;
     }
 
-    public void procesarMensajes() throws InterruptedException {
+    public void procesarMensaje() throws InterruptedException {
         Boolean i = mailbox.retirar();
         if (i) {
             vecinosVivos++;
@@ -48,16 +55,25 @@ public class Cell extends Thread{
     public void run()  {
         try {
             for (int i = 0; i < numeroGeneraciones; i++) {
-                for (Mailbox vecino : vecinos) {
-                    vecino.almacenar(estado);
-                    procesarMensajes();
+                if (rol%2 == 0){
+                    for (Mailbox vecino : vecinos) {
+                        vecino.almacenar(estado);
+                        procesarMensaje();
+                    }
                 }
-                System.out.println("Esperando en la barrera de estado " + estado + " " + vecinosVivos);
+                else {
+                    for (Mailbox vecino : vecinos) {
+                        procesarMensaje();
+                        vecino.almacenar(estado);
+                    }
+                }
+   
+                //System.out.println("Esperando en la barrera de estado ");
                 barreraEstado.await();
                 calcularEstado();
                 vecinosVivos = 0;
 
-                System.out.println("Esperando en la barrera de generacion");
+                //System.out.println("Esperando en la barrera de generacion");
                 barreraGeneracion.await();
             }
             Cell.fin = true;
